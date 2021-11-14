95883d2575b7d40e00912225c95afe1b1712b6b7
Franz Medrano <franz.medrano@icloud.com>
2021-11-14 12:24:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const pa11y = require("pa11y");
+const express = require("express");
+const PORT = process.env.port || 5000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/test", async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: "url is required" });
+  } else {
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
